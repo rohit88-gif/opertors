2f74e8f028f47c943dee925b7737914820dd367d
rohit88-gif <rohit1416shinde@gmail.com>
2025-08-20 12:37:27 +0530

    Add files via upload

---
+++ operaors.c
@@ -0,0 +1,21 @@
+
+
+int main()
+{
+	int num1,num2, sum,sub,mul,div;
+	printf("enter the 1st number");
+	scanf("%d",&num1);
+	
+	printf("enter the 2nd number");
+	scanf("%d",&num2);
+	
+	sum=num1+num2;
+	sub=num1-num2;
+	mul=num1*num2;
+	div=num1/num2;
+	
+	printf(" Sum %d",sum);
+	printf(" sub %d", sub);
+	printf("mul %d",mul);
+	printf("div %d",div);
+}
